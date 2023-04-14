7b6edd194a40c1a0549ef9dc8a3f93d16057a86f
Q2CK <109765887+Q2CK@users.noreply.github.com>
2023-04-14 16:42:49 +0200

    fixed overflow

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ mod bit_ops;
 use bit_ops::*;
 
 fn main() {
-    let data1 = DataWord::from(5, 8);
-    let data2 = DataWord::from(8, 8);
+    let data1 = DataWord::from(127, 8);
+    let data2 = DataWord::from(1, 8);
     println!("{:?}", data1 + data2);
     println!("{:?}", data1 - data2);
     println!("{:?}", data1 & data2);
